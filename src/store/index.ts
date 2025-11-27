6bc87a15c77b1dc50b26b5c956c7656a3beefc36
misticslippers <eternal.martyr13579@gmail.com>
2025-11-27 10:37:26 +0300

    add index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import routesReducer from './routesSlice';
 import locationsReducer from './locationsSlice';
 import coordinatesReducer from './coordinatesSlice';
+import importsReducer from './importsSlice';
+import authReducer from './authSlice';
 
 export const store = configureStore({
     reducer: {
         routes: routesReducer,
         locations: locationsReducer,
         coordinates: coordinatesReducer,
+        imports: importsReducer,
+        auth: authReducer,
     },
 });
 
